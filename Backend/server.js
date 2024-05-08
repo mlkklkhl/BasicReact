0aa94263f45492bed37208d1eb56d4b3a8660321
Mallika Kliangkhlao <42213474+mlkklkhl@users.noreply.github.com>
2024-05-08 13:44:51 +0700

    Update server.js debug mysql2

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2');
 const config = require('./config');
 const express = require('express');
 const cors = require('cors');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 const app = express();
 
 const port = config.express.port;
@@ -18,11 +20,11 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('Hello World! Let\'s Working with NoSQL Databases')
+	res.send('Hello World! Let\'s Working with SQL Databases')
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Test Example app listening at http://localhost:${port}`)
 })
 
 const connectDB = async () => {
@@ -44,39 +46,36 @@ connectDB();
 
 // Read Patients All API
 app.get('/patients', async (req, res) => {
-	var readSQL = "SELECT * FROM patients";
-
-	try {
-		await con.query(readSQL, function (err, results) {
+	await new Promise((resolve, rejects) => {
+		var readSQL = "SELECT * FROM patients";
+		con.query(readSQL, (err, results) => {
 			if (err) {
 				console.log('database connection error!, ', err);
 			} else {
 				res.status(200).send(results);
 			}
 		});
-	} catch (err) {
-		console.log(err);
-		res.status(404).send("backend error!");
-	}
+	});
 });
 
 
 // Read Rights All API
 app.get('/rights', async (req, res) => {
 	var readSQL = "SELECT distinct Patient_Rights FROM rights;";
-
-	try {
-		await con.query(readSQL, function (err, results) {
-			if (err) {
-				console.log('database connection error!, ', err);
-			} else {
-				res.status(200).send(results);
-			}
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(404).send("backend error!");
-	}
+	await new Promise((resolve, rejects) => {
+		try {
+			con.query(readSQL, function (err, results) {
+				if (err) {
+					console.log('database connection error!, ', err);
+				} else {
+					res.status(200).send(results);
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(404).send("backend error!");
+		}
+	});
 });
 
 // Login
@@ -86,20 +85,21 @@ app.post('/login/', async (req, res) => {
 	const password = params.pass;
 
 	var loginSQL = "SELECT * FROM users WHERE username='" + username + "' and password='" + password + "';"
-
-	try {
-		await con.query(loginSQL, function (err, results) {
-			if (err) {
-				console.log('database connection error!, ', err);
-				res.status(404).send("database connection error!");
-			} else {
-				res.status(200).send(results);
-			}
-		});
-	} catch (err) {
-		console.log(err);
-		process.exit(1);
-	}
+	await new Promise((resolve, rejects) => {
+		try {
+			con.query(loginSQL, function (err, results) {
+				if (err) {
+					console.log('database connection error!, ', err);
+					res.status(404).send("database connection error!");
+				} else {
+					res.status(200).send(results);
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			process.exit(1);
+		}
+	});
 });
 
 // Read Patient by Name
@@ -112,24 +112,25 @@ app.post('/patients/:searchText', async (req, res) => {
 
 	var searchSQL = "SELECT * FROM patients WHERE Name LIKE '%" + searchText + "%';"
 
-	if (test) {
-		res.status(200).send();
-	}else{
-		try {
-			await con.query(searchSQL, function (err, results) {
-				if (err) {
-					console.log('database connection error!, ', err);
-					res.status(404).send("backend error!");
-				} else {
-					res.status(200).send(results);
-				}
-			});
-		} catch (err) {
-			console.log(err);
-			res.status(404).send("backend error!");
+	await new Promise((resolve, rejects) => {
+		if (test) {
+			res.status(200).send();
+		} else {
+			try {
+				con.query(searchSQL, function (err, results) {
+					if (err) {
+						console.log('database connection error!, ', err);
+						res.status(404).send("backend error!");
+					} else {
+						res.status(200).send(results);
+					}
+				});
+			} catch (err) {
+				console.log(err);
+				res.status(404).send("backend error!");
+			}
 		}
-	}
-
+	});
 });
 
 // Read Patient by ID
@@ -139,22 +140,24 @@ app.post('/queryid', async (req, res) => {
 	const patientID = params.patientID
 	var searchSQL = "SELECT * FROM patients WHERE ID = " + patientID + ";";
 
-	try {
-		await con.query(searchSQL, function (err, results) {
-			if (err) {
-				console.log('database connection error!, ', err);
-				res.status(200).send(results);
-			} else if (results.length > 0) {
-				res.status(200).send(results);
-			} else {
-				console.log('no query!');
-				res.status(404).send("no query!");
-			}
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(404).send("backend error!");
-	}
+	await new Promise((resolve, rejects) => {
+		try {
+			con.query(searchSQL, function (err, results) {
+				if (err) {
+					console.log('database connection error!, ', err);
+					res.status(200).send(results);
+				} else if (results.length > 0) {
+					res.status(200).send(results);
+				} else {
+					console.log('no query!');
+					res.status(404).send("no query!");
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(404).send("backend error!");
+		}
+	});
 });
 
 // // Create API
@@ -163,19 +166,20 @@ app.post('/create/', async (req, res) => {
 
 	var insertSQL = "INSERT INTO patients (HN, Name, Patient_Rights_1, Patient_Rights_2, Patient_Rights_3, Chronic_Disease, Address, Phone) VALUES ('" + params.HN + "', '" + params.Name + "', '" + params.Patient_Rights_1 + "', '" + params.Patient_Rights_2 + "', '" + params.Patient_Rights_3 + "', '" + params.Chronic_Disease + "', '" + params.Address + "', '" + params.Phone + "');";
 
-	console.log(params);
-	try {
-		await con.query(insertSQL, function (err) {
-			if (err) {
-				console.log('database connection error!, ', err);
-			} else {
-				res.status(200).send('insert success');
-			}
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(404).send("backend error!");
-	}
+	await new Promise((resolve, rejects) => {
+		try {
+			con.query(insertSQL, function (err) {
+				if (err) {
+					console.log('database connection error!, ', err);
+				} else {
+					res.status(200).send('insert success');
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(404).send("backend error!");
+		}
+	});
 });
 
 
@@ -186,18 +190,20 @@ app.put('/patients/update/', async (req, res) => {
 	const patientID = params.patientID;
 	var updateSQL = "UPDATE patients SET HN = '" + params.HN + "', Name = '" + params.Name + "', Patient_Rights_1 = '" + params.Patient_Rights_1 + "', Patient_Rights_2 = '" + params.Patient_Rights_2 + "', Patient_Rights_3 = '" + params.Patient_Rights_3 + "', Chronic_Disease= '" + params.Chronic_Disease + "', Address = '" + params.Address + "', Phone ='" + params.Phone + "' WHERE (ID = '" + patientID + "');";
 
-	try {
-		await con.query(updateSQL, function (err) {
-			if (err) {
-				console.log('database connection error!, ', err);
-			} else {
-				res.status(200).send();
-			}
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(404).send("backend error!");
-	}
+	await new Promise((resolve, rejects) => {
+		try {
+			con.query(updateSQL, function (err) {
+				if (err) {
+					console.log('database connection error!, ', err);
+				} else {
+					res.status(200).send();
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(404).send("backend error!");
+		}
+	});
 });
 
 
@@ -209,18 +215,20 @@ app.delete('/patients/delete/', async (req, res) => {
 	const patientID = params.patientID;
 	var deleteSQL = "DELETE FROM patients WHERE ID = " + patientID + ";";
 
-	try {
-		await con.query(deleteSQL, function (err) {
-			if (err) {
-				console.log('database connection error!, ', err);
-			} else {
-				res.status(200).send();
-			}
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(404).send("backend error!");
-	}
+	await new Promise((resolve, rejects) => {
+		try {
+			con.query(deleteSQL, function (err) {
+				if (err) {
+					console.log('database connection error!, ', err);
+				} else {
+					res.status(200).send();
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(404).send("backend error!");
+		}
+	});
 });
 
 //if cannot connect
